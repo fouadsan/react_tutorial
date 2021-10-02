7431e901d40512b53cd8cb526decad3338506793
fouadsan <f.benayad95@gmail.com>
2021-10-02 15:18:43 +0100

    Props

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -3,27 +3,46 @@ import ReactDOM from "react-dom";
 
 // CSS
 import './index.css';
+// setup vars
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/71Ptolb7qnL._AC_UL160_SR160,160_.jpg",
+  title: "Bob Woodward",
+  author: 'Peril',
+}
+
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81X+TLMA8oL._AC_UL160_SR160,160_.jpg",
+  title: "The Sandman: Act II",
+  author: 'Neil Gaiman',
+}
+
+
 function BookList() {
   return (
     <section className='booklist'> 
-      <Book />
+      <Book 
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+       <Book 
+         img={secondBook.img} 
+         title={secondBook.title}
+         author={secondBook.author}
+        />
     </section>
   );
 }
 
-const author = 'Peril';
-
-const Book = () => {
-  const title = 'Bob Woodward'
+const Book = (props) => {
+  console.log(props);
   return (
     <article className='book'>
       <img
-        src="https://images-na.ssl-images-amazon.com/images/I/71Ptolb7qnL._AC_UL160_SR160,160_.jpg"
-        alt=""
-      />
-      <h4>{title}</h4>
-      <h1>{author.toUpperCase()}</h1>
-      <p>{6 + 6}</p>
+        src={props.img} alt="" />
+      <h4>{props.title}</h4>
+      <h1>{props.author}</h1>
+      
     </article>
   );
 };
