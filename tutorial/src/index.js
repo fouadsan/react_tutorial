5ca0fb030a45211a4f2f82d194e687933132589e
fouadsan <f.benayad95@gmail.com>
2021-10-02 10:22:23 +0100

    JSX Javascript

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -7,34 +7,25 @@ function BookList() {
   return (
     <section className='booklist'> 
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
 
+const author = 'Peril';
+
 const Book = () => {
+  const title = 'Bob Woodward'
   return (
     <article className='book'>
-      <Image></Image>
-      <Title />
-      <Author />
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/71Ptolb7qnL._AC_UL160_SR160,160_.jpg"
+        alt=""
+      />
+      <h4>{title}</h4>
+      <h1>{author.toUpperCase()}</h1>
+      <p>{6 + 6}</p>
     </article>
   );
-};
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/71Ptolb7qnL._AC_UL160_SR160,160_.jpg"
-    alt=""
-  />
-);
-const Title = () => <h1>Peril</h1>;
-const Author = () => {
-  return <h4 style={{color:'#617d98', fontSize: '0.75rem', margin: '0.25rem'}}>Bob Woodward</h4>;
 };
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
