45b008a12639ac6a55729c6d8cd988e8a05246d7
fouad benayad <fouad.benayad@bioreal.local>
2021-10-04 16:37:01 +0100

    Hooks

--- react-advanced/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ react-advanced/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,29 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  }
+
+  useEffect(() => {
+    console.log('useEffect')
+    window.addEventListener('resize', checkSize);
+    return () => {
+      console.log('cleanup');
+      window.removeEventListener('resize', checkSize);
+    }
+  });
+
+  console.log('render')
+
+  return (
+    <React.Fragment>
+      <h1>window</h1>
+      <h2>{size} Pixels</h2>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectCleanup;
