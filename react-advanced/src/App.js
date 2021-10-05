f10de004f7a64611fae6e4af3fc1bfd3e73a515e
fouad benayad <fouad.benayad@bioreal.local>
2021-10-05 16:48:22 +0100

    Hooks

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
+import Setup from './tutorial/6-useReducer/setup';
 
 function App() {
   return (
     <div className='container'>
       <Setup />
     </div>
-  )
+  );
 }
 
 export default App
